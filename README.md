3e4edc12862cba6082864bdefc963e5a65d2e160
Amaris Dupré <amaris.blecinc@gmail.com>
2025-07-06 05:22:33 -0300

    commit limpo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto Blecinc
+
+Loja moderna, inteligente e automatizada com Next.js
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Bem-vindo à loja Blecinc 🛍️</h1>
+      <p>Este é o início de algo incrível.</p>
+    </div>
+  );
+}
